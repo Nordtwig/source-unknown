08e3bc88116bd6a62fc71bf83bd81809ac6abbbc
Noah <nordqvist.gaming@gmail.com>
2023-11-24 19:56:03 +0100

    Add post route for new users

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dbConf from "./db_config.json" assert { type: "json" };
 import ejs from "ejs";
 
 const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: dbConf.database,
+    password: dbConf.password,
+    port: dbConf.port
+})
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -24,6 +35,19 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.post("/register", async (req, res) => {
+    console.log(req.body);
+    const user = await db.query({
+        text: "INSERT INTO users(email, password) VALUES($1, $2)",
+        values: [req.body.username, req.body.password]
+    }).catch(error => {
+        console.log("Query Error: " + error);
+    }).finally(() => {
+        console.log("Reached finally");
+        res.redirect("/");
+    })   
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
 });
