4041ecdfe3d0f489df23b25d74c7d0f7920aebc9
Noah <nordqvist.gaming@gmail.com>
2023-11-24 18:59:36 +0100

    Set up basic routes

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 //jshint esversion:6
+
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
